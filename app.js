17bc469ecb2dacba457458c497d7685fc17aa722
RodriNegron <rodrigoo.n@hotmail.com.ar>
2021-01-14 19:07:57 -0300

    formularios agregados

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ app.get("/register", (req,res)=>
 {
     res.sendFile(__dirname + "/views/register.html");
 })
+
+app.get("/login", (req,res)=>
+{
+    res.sendFile(__dirname + "/views/login.html");
+})
